46d36dcca035b93682f67ba9dae69e9fabbb5a2e
Bine Divyesh <156177765+B-Divyesh@users.noreply.github.com>
2026-08-28 20:03:50 +0530

    fix v0.1.1 formula content

--- Formula/installer-release-doctor.rb
+++ Formula/installer-release-doctor.rb
@@ -1,36 +1,21 @@
-{
-  "url": "https://api.github.com/repos/B-Divyesh/sf-installer-release-doctor/releases/assets/533828278",
-  "id": 533828278,
-  "node_id": "RA_kwDOUG_W2M4f0ZK2",
-  "name": "installer-release-doctor.rb",
-  "label": "",
-  "uploader": {
-    "login": "github-actions[bot]",
-    "id": 41898282,
-    "node_id": "MDM6Qm90NDE4OTgyODI=",
-    "avatar_url": "https://avatars.githubusercontent.com/in/15368?v=4",
-    "gravatar_id": "",
-    "url": "https://api.github.com/users/github-actions%5Bbot%5D",
-    "html_url": "https://github.com/apps/github-actions",
-    "followers_url": "https://api.github.com/users/github-actions%5Bbot%5D/followers",
-    "following_url": "https://api.github.com/users/github-actions%5Bbot%5D/following{/other_user}",
-    "gists_url": "https://api.github.com/users/github-actions%5Bbot%5D/gists{/gist_id}",
-    "starred_url": "https://api.github.com/users/github-actions%5Bbot%5D/starred{/owner}{/repo}",
-    "subscriptions_url": "https://api.github.com/users/github-actions%5Bbot%5D/subscriptions",
-    "organizations_url": "https://api.github.com/users/github-actions%5Bbot%5D/orgs",
-    "repos_url": "https://api.github.com/users/github-actions%5Bbot%5D/repos",
-    "events_url": "https://api.github.com/users/github-actions%5Bbot%5D/events{/privacy}",
-    "received_events_url": "https://api.github.com/users/github-actions%5Bbot%5D/received_events",
-    "type": "Bot",
-    "user_view_type": "public",
-    "site_admin": false
-  },
-  "content_type": "application/octet-stream",
-  "state": "uploaded",
-  "size": 854,
-  "digest": "sha256:478a657678078105d8ee1d46d48f4a40f708f4047b4ed243f571c241de8c102d",
-  "download_count": 0,
-  "created_at": "2026-08-28T13:16:01Z",
-  "updated_at": "2026-08-28T13:16:02Z",
-  "browser_download_url": "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.1/installer-release-doctor.rb"
-}
+class InstallerReleaseDoctor < Formula
+  desc "Check installer artifacts before release channels reject them"
+  homepage "https://installer-release-doctor.sociobot.in"
+  version "0.1.1"
+  license "MIT"
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.1/release-doctor-v0.1.1-darwin-aarch64.tar.gz"
+      sha256 "c52de2728ddd161a567f919826f00f5ed5e91a91f4138ca45aaba7b997949d96"
+    else
+      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.1/release-doctor-v0.1.1-darwin-x86_64.tar.gz"
+      sha256 "c9ae54de1ac68c4cb40c8f3311f644e55e86d8a2ae9b3aa0291a5f4355291cad"
+    end
+  end
+  def install
+    bin.install "release-doctor"
+  end
+  test do
+    assert_match version.to_s, shell_output(bin/"release-doctor" + " --version")
+  end
+end
