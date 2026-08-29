a99bf359db1f7d72617e28099d1a45da244d837e
Bine Divyesh <156177765+B-Divyesh@users.noreply.github.com>
2026-08-29 04:40:39 +0000

    publish formula v0.1.2

--- Formula/installer-release-doctor.rb
+++ Formula/installer-release-doctor.rb
@@ -1,15 +1,15 @@
 class InstallerReleaseDoctor < Formula
   desc "Check installer artifacts before release channels reject them"
   homepage "https://installer-release-doctor.sociobot.in"
-  version "0.1.1"
+  version "0.1.2"
   license "MIT"
   on_macos do
     if Hardware::CPU.arm?
-      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.1/release-doctor-v0.1.1-darwin-aarch64.tar.gz"
-      sha256 "c52de2728ddd161a567f919826f00f5ed5e91a91f4138ca45aaba7b997949d96"
+      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.2/release-doctor-v0.1.2-darwin-aarch64.tar.gz"
+      sha256 "4b959e00c3ef5614ce0de6cf1a673cfb123f8ec5a5450e3f4d290490eecfd4b9"
     else
-      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.1/release-doctor-v0.1.1-darwin-x86_64.tar.gz"
-      sha256 "c9ae54de1ac68c4cb40c8f3311f644e55e86d8a2ae9b3aa0291a5f4355291cad"
+      url "https://github.com/B-Divyesh/sf-installer-release-doctor/releases/download/v0.1.2/release-doctor-v0.1.2-darwin-x86_64.tar.gz"
+      sha256 "3286275a877d93e95e8bfc68aca6e6cfb388221fb94500bb2172e187381f35bb"
     end
   end
   def install
